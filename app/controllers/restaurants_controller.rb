7629c44c249dc7cbab44cb2391c77cfb32b27782
Al <pursuance@gmail.com>
2015-02-18 17:24:11 +0100

    restaurant completed

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,40 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  # GET /restaurants
+  # GET /restaurants.json
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+    end
+end
